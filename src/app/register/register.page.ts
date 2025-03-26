d02adb37cfeea2482007ca1e10ad15b7833de795
DiogoPCS <diogo.paulino.santos@hotmail.com>
2025-03-26 15:14:04 -0300

    teste

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../services/message.service';
+import { CrudService } from '../services/crud.service';
+import { AuthenticateService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  user: any = {
+    email: null,
+    password: null,
+    confirm_password: null
+  }
+
+  constructor(
+    private messageService: MessageService,
+    private crudService: CrudService,
+    public authService: AuthenticateService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  criar(){
+    if (this.user.password != this.user.confirm_password) {
+      this.messageService.show('As senhas não conferem!', 3000)
+      return;
+    }
+
+    this.authService.register(this.user.email, this.user.password)
+    .then(response => {
+      if (response) 
+        this.router.navigate(['/login']);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log('Requisição finalizada!');
+    })
+
   }
 
 }
